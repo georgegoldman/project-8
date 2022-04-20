48dfe14f4808b29b3a816c66b5daa5ec88041d37
onyedikachi <onyedikachi@localhost.localdomain>
2022-04-20 16:40:10 +0100

    added the register page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
+  hide:boolean = true
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  username = new FormControl('', [Validators.required])
+  password = new FormControl('', Validators.required)
+
+  getEmailErrorMessage():string {
+    if(this.email.hasError('email')) return 'Please enter a valid email'
+    return this.email.hasError('required') ? 'You must enter a email': '';
+  }
+
+  getUsernameErrorMessage():string {
+    return this.username.hasError('required') ? 'You must enter a Username' : ''
+  }
+
+  getPasswordErrorMessage():string {
+    return this.password.hasError('required') ? 'You must provide a password' : ''
+  }
 
   constructor() { }
 
