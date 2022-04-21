7f72bcba89f05bf39fc12af6723fd1b2effbc0e5
onyedikachi <onyedikachi@localhost.localdomain>
2022-04-21 17:23:36 +0100

    rm payment-service and mk payment

--- src/app/airtime-page/airtime-page.component.ts
+++ src/app/airtime-page/airtime-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { HomeComponent } from '../home/home.component';
+import { Airtime } from './airtimeInt';
 
 @Component({
   selector: 'app-airtime-page',
@@ -7,9 +9,12 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./airtime-page.component.css']
 })
 export class AirtimePageComponent implements OnInit {
-
-
+  form: FormGroup;
+  airTimeProviders: Airtime[] = []
+  airtime  = new HomeComponent()
+  airtime.typesOfServices
   constructor() { }
+
 
   ngOnInit(): void {
   }
